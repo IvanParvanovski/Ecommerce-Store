0321da556ca4b4b3ad43513dbb5d39385f7a8605
Ivan Parvanovski <iparvanovski@gmail.com>
2022-12-06 21:13:51 +0200

    User interface implemented

---
+++ app/src/app/shared/interfaces/user.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+    __id: string;
+    firstName: string,
+    lastName: string,
+    password: string,
+    email: string,
+    created_at: string,
+    updated_at: string,
+    __v: number;
+}
