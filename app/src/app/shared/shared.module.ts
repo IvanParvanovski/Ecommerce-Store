6d850131ccfef5a567b566518124ce3df37aeb2b
Ivan Parvanovski <iparvanovski@gmail.com>
2022-12-10 17:10:10 +0200

    Format the price to the second decimal point using PIPE

--- app/src/app/shared/shared.module.ts
+++ app/src/app/shared/shared.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { HighlightLinkDirectiveDirective } from '../core/nav-bar/highlight-link-directive.directive';
+import { PriceFormatPipe } from './price-format.pipe';
 
 
 
 @NgModule({
   declarations: [
-    HighlightLinkDirectiveDirective
+    HighlightLinkDirectiveDirective,
+    PriceFormatPipe
   ],
   imports: [
     CommonModule,
   ],
+  exports: [
+    PriceFormatPipe
+  ]
 })
 
 export class SharedModule { }
